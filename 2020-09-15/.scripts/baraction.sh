30af20f5713ad38fd078bb38277329f38a190861
TCH69 <ifa26417@outlook.com.vn>
2020-09-15 13:06:28 +0700

    added rice

---
+++ 2020-09-15/.scripts/baraction.sh
@@ -0,0 +1,25 @@
+#!/usr/local/bin/bash
+## SpectrWM bar
+
+disk() {
+	free=$(df -h | awk 'NR==2 {print $1" "$3"/"$2" "$5" "$4}')
+	echo -e $free
+}
+
+vol() {
+	left=$(amixer get Master | awk 'NR==6 {print $4"%"}')
+	right=$(amixer get Master | awk 'NR==7 {print $4"%"}')
+	echo -e "VOL: $left|$right"
+}
+
+engine() {
+	engine=$($HOME/.scripts/ibus-engine.sh)
+	echo -e $engine
+}
+
+sleep_sec=0.1
+
+while :; do
+	echo "$(disk) | $(vol) | $(engine) |"
+	sleep $sleep_sec
+done
