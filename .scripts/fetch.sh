de7fcdd067695e1ed2005590cc5043ff87b5cb02
tch69 <44942508+TCH69@users.noreply.github.com>
2020-10-28 14:02:21 +0700

    Update fetch.sh

--- .scripts/fetch.sh
+++ .scripts/fetch.sh
@@ -1,11 +1,11 @@
-#!/usr/local/bin/zsh
+#!/bin/bash
 ## Dummy info script
 
 # Color used by the script
 color=$(echo $((RANDOM % 256 +1 )))
 
 # OS/Distro using
-os=$(uname)
+os=$(awk '/^NAME/ {print $0}' /etc/os-release | cut -f 2 -d '"')
 
 # OS/Kernel version
 ver=$(uname -r)
@@ -14,7 +14,7 @@ ver=$(uname -r)
 wm=$(wmctrl -m | head -n1)
 
 # Amount of packages installed
-pack=$(ls /var/db/pkg | gwc -l)
+pack=$(qlist -I | wc -l)
 
 ## Script, start, NOW!
 clear
@@ -29,8 +29,8 @@ echo -e '\e[1;38;5;'$color'm         Kernel:\e[0m '${SHELL##*/}''
 echo -e '\e[1;38;5;'$color'm             WM:\e[0m '${wm##* }'	'
 echo -e '\e[1;38;5;'$color'm       Terminal:\e[0m '$TERM'	'
 echo -e ''
-echo -e '     '
-echo -e '     \e[1;90m███\e[1;91m███\e[1;92m███\e[1;93m███\e[1;94m███\e[1;95m███\e[1;96m███\e[1;39m███'
+echo -e '     \e[1;30m███\e[1;31m███\e[1;32m███\e[1;33m███\e[1;34m███\e[1;35m███\e[1;36m███\e[1;37m███'
+echo -e '     \e[1;90m███\e[1;91m███\e[1;92m███\e[1;93m███\e[1;94m███\e[1;95m███\e[1;96m███\e[1;97m███'
 echo -e ''
 
 unset color
