b8f1b56f32f6c8fd9571365b60b19c749809c290
Pham Ngoc-Dung <44942508+tch69@users.noreply.github.com>
2024-10-31 15:28:58 +0700

    Thêm slock

---
+++ slock/config.def.h
@@ -0,0 +1,28 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* Kerning / character bounding-box multipliers */
+static float cwscale = 1.0;
+static float chscale = 1.0;
+
+/* lock screen opacity */
+static const float alpha = 1;
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "sans-serif:size:pixelsize=24:antialias=true:autohint=true";
